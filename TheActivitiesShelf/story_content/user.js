ba4c8f36dcbe25666d0917e7c81c6d46c96a6e29
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-04-03 17:28:36 +0300

    Add files via upload

---
+++ TheActivitiesShelf/story_content/user.js
@@ -0,0 +1,143 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6dsaFgG20qT":
+        Script1();
+        break;
+      case "6m8m6NCzkJx":
+        Script2();
+        break;
+      case "6PWEzyxgTtM":
+        Script3();
+        break;
+      case "6QMnMT807YB":
+        Script4();
+        break;
+      case "66pOvBXzASs":
+        Script5();
+        break;
+      case "6nn50riJ6TQ":
+        Script6();
+        break;
+      case "5derHtAiNLO":
+        Script7();
+        break;
+      case "6PPCoSsfeEv":
+        Script8();
+        break;
+      case "6qiR3uMpNw1":
+        Script9();
+        break;
+      case "62xPLl4DVNq":
+        Script10();
+        break;
+      case "6pHwzIZMh8f":
+        Script11();
+        break;
+      case "5fTRTgIUVjs":
+        Script12();
+        break;
+      case "5xGdvePlxch":
+        Script13();
+        break;
+      case "6CLJLYTZH15":
+        Script14();
+        break;
+      case "6rdYxRxZmzF":
+        Script15();
+        break;
+      case "6dQQlzXBNkL":
+        Script16();
+        break;
+      case "5cW6XS6CLwH":
+        Script17();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
+function Script5()
+{
+  window.print();
+}
+
+function Script6()
+{
+  window.print();
+}
+
+function Script7()
+{
+  window.print();
+}
+
+function Script8()
+{
+  window.print();
+}
+
+function Script9()
+{
+  window.print();
+}
+
+function Script10()
+{
+  window.print();
+}
+
+function Script11()
+{
+  window.print();
+}
+
+function Script12()
+{
+  window.print();
+}
+
+function Script13()
+{
+  window.print();
+}
+
+function Script14()
+{
+  window.print();
+}
+
+function Script15()
+{
+  window.print();
+}
+
+function Script16()
+{
+  window.print();
+}
+
+function Script17()
+{
+  window.print();
+}
+
