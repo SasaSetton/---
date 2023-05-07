92c63f4d25b56a190f94cf3e440ea3552d986ec9
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-05-07 12:30:49 +0300

    Add files via upload

--- TheActivitiesShelf/Canvas/jScripts/JavaScript.js
+++ TheActivitiesShelf/Canvas/jScripts/JavaScript.js
@@ -1,35 +1,76 @@
-﻿const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let coord = { x: 0, y: 0 };
+﻿// Instagram.com/kamyar_lajani
+// https://github.com/KamyarLajani
+var ctx, canvas;
+new Vue({
+    el: '#app',
 
-document.addEventListener("mousedown", start);
-document.addEventListener("mouseup", stop);
-window.addEventListener("resize", resize);
 
-resize();
+    // Assigning canvas element
+    mounted() {
+        canvas = this.$refs.canvas;
+        ctx = canvas.getContext('2d');
+    },
+    data: {
+        x: 0,
+        y: 0,
+        mDown: false,
+        pen: true,
+        penSizeShow: false,
+        penSize: 5,
+        clean: false,
+        cleanSize: 20,
+        color: 'black',
+        save: false,
+        imageType: 'image/jpg',
+        dataUrl: ''
 
-function resize() {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-}
-function reposition(event) {
-    coord.x = event.clientX - canvas.offsetLeft;
-    coord.y = event.clientY - canvas.offsetTop;
-}
-function start(event) {
-    document.addEventListener("mousemove", draw);
-    reposition(event);
-}
-function stop() {
-    document.removeEventListener("mousemove", draw);
-}
-function draw(event) {
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "#ACD3ED";
-    ctx.moveTo(coord.x, coord.y);
-    reposition(event);
-    ctx.lineTo(coord.x, coord.y);
-    ctx.stroke();
-}
+
+    },
+    methods: {
+
+        mouseDown: function () {
+            this.mDown = true;
+            ctx.beginPath();
+            ctx.lineWidth = this.penSize;
+            ctx.strokeStyle = this.color;
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+            ctx.moveTo(this.x, this.y);
+
+        },
+
+        mouseUp: function () {
+            this.mDown = false
+
+        },
+
+        mouseMove: function (e) {
+            // Mouse down should be fire before mouse move
+            if (this.mDown) {
+                this.x = e.offsetX;
+                this.y = e.offsetY;
+
+                if (this.clean) {
+
+                    ctx.clearRect(this.x, this.y, this.cleanSize, this.cleanSize);
+
+                }
+                else {
+                    ctx.lineTo(this.x, this.y);
+                    ctx.stroke();
+                }
+            }
+
+        },
+        // reset all
+        clear: function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        },
+        // canvas to Data URL (base64) then adding it in href of download button to be downloadable with base64
+        saving: function () {
+            this.dataUrl = canvas.toDataURL(this.imageType);
+            console.log(this.dataUrl);
+        }
+    }
+});
+
