fa8b2a16d24f5b2ae5fd1b2b9b9c60fe5448d992
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-05-09 21:28:07 +0300

    Add files via upload

--- TheActivitiesShelf/story_content/user.js
+++ TheActivitiesShelf/story_content/user.js
@@ -2,55 +2,55 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5ePYp8VzBvT":
+      case "6LqO8UhmtUa":
         Script1();
         break;
-      case "691ER8s2vI6":
+      case "5zs5EKeGbP6":
         Script2();
         break;
-      case "5mjCvyBWw8M":
+      case "61UPwOcbNgt":
         Script3();
         break;
-      case "6FfKVVfo35l":
+      case "6bM2WrP7SS3":
         Script4();
         break;
-      case "6BlDea3eNVB":
+      case "5r4JwsBr4TO":
         Script5();
         break;
-      case "6TzswHTVCsm":
+      case "6jHwiOWIqEz":
         Script6();
         break;
-      case "6UDYiS8Bj16":
+      case "6hmnV9sqs1I":
         Script7();
         break;
-      case "6LH5kM4kMpv":
+      case "5ybLTw0plZU":
         Script8();
         break;
-      case "6l7PlA9hToj":
+      case "5YfcDCTCP6U":
         Script9();
         break;
-      case "5iVcmNnrTy8":
+      case "6H8IwXQf82G":
         Script10();
         break;
-      case "69N0ngFoTaz":
+      case "6La7bKcTVEp":
         Script11();
         break;
-      case "67tR9J34Uib":
+      case "6WFU9q3S6F0":
         Script12();
         break;
-      case "6e3xpHGNgFz":
+      case "6ngUVZr71Ub":
         Script13();
         break;
-      case "6aEAN1RxQq9":
+      case "6dcLqiGHuM6":
         Script14();
         break;
-      case "6KGrgAxabth":
+      case "5rWRonYGFxx":
         Script15();
         break;
-      case "6Hhc088d9rX":
+      case "6lInoofAskx":
         Script16();
         break;
-      case "5pxOJUorjZZ":
+      case "6Kqog0B7T2n":
         Script17();
         break;
   }
