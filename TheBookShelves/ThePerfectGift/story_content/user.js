4abb14b86b0a2bf47818ce87c26aa3c175a616b4
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-09-03 12:48:53 +0300

    Add files via upload

---
+++ TheBookShelves/ThePerfectGift/story_content/user.js
@@ -0,0 +1,87 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6DcQsVwfYqV":
+        Script1();
+        break;
+      case "69NxvNPDpm0":
+        Script2();
+        break;
+      case "6jjnezYLCGE":
+        Script3();
+        break;
+      case "6YbxhNy0Sys":
+        Script4();
+        break;
+      case "5c1BIiueGSk":
+        Script5();
+        break;
+      case "6f0sMh145a8":
+        Script6();
+        break;
+      case "6XQ982FQW4Z":
+        Script7();
+        break;
+      case "5ltreR5nfCd":
+        Script8();
+        break;
+      case "5fulCiJzN1H":
+        Script9();
+        break;
+      case "5duHiMB6CjN":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
+function Script5()
+{
+  window.print();
+}
+
+function Script6()
+{
+  window.print();
+}
+
+function Script7()
+{
+  window.print();
+}
+
+function Script8()
+{
+  window.print();
+}
+
+function Script9()
+{
+  window.print();
+}
+
+function Script10()
+{
+  window.print();
+}
+
